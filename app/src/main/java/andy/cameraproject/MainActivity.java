efdae990ebd17ec342e067131848006b551b4087
AndrewDongAndy <dongandrew99@gmail.com>
2017-12-26 14:35:45 -0500

    Username/password more-or-less works.

--- app/src/main/java/andy/cameraproject/MainActivity.java
+++ app/src/main/java/andy/cameraproject/MainActivity.java
@@ -12,52 +12,44 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnStart;
-    WebView webCamera;
-    EditText editUsername;
-    EditText editPassword;
-    EditText editStreamURL;
-
     Handler timer;
     int delay;
+    int fps;
 
-    String webURL;
-    String username;
-    String password;
+    private String webURL;
+    private String username;
+    private String password;
+
+    private WebViewClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize views
-        btnStart = findViewById(R.id.btnStart);
-        webCamera = findViewById(R.id.webCamera);
-        editUsername = findViewById(R.id.editUsername);
-        editPassword = findViewById(R.id.editPassword);
-        editStreamURL = findViewById(R.id.editStreamURL);
+        // Initialize views as finals
+        final Button btnStart = findViewById(R.id.btnStart);
+        final WebView webCamera = findViewById(R.id.webCamera);
+        final EditText editUsername = findViewById(R.id.editUsername);
+        final EditText editPassword = findViewById(R.id.editPassword);
+        final EditText editStreamURL = findViewById(R.id.editStreamURL);
 
         // Prepare image streamer
         timer = new Handler();
-        delay = 200;
+        fps = 10;
+        delay = 1000 / fps;
         webCamera.setInitialScale(210);
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
+                // Get current states
                 username = editUsername.getText().toString();
                 password = editPassword.getText().toString();
                 webURL = editStreamURL.getText().toString();
 
-                webCamera.setWebViewClient(new WebViewClient() {
-                    @Override
-                    public void onReceivedHttpAuthRequest(WebView view, HttpAuthHandler handler, String host, String realm) {
-                        super.onReceivedHttpAuthRequest(view, handler, host, realm);
-                        handler.proceed(username, password);
-                    }
-                });
+                client = new MyWebViewClient(username, password);
+                webCamera.setWebViewClient(client);
 
                 timer.postDelayed(new Runnable() {
                     @Override
@@ -68,5 +60,21 @@ public class MainActivity extends AppCompatActivity {
                 }, delay);
             }
         });
+    }
+}
+
+class MyWebViewClient extends WebViewClient {
+    private String handlerUsername;
+    private String handlerPassword;
+
+
+    public MyWebViewClient(String username, String password) {
+        handlerUsername = username;
+        handlerPassword = password;
+    }
+
+    @Override
+    public void onReceivedHttpAuthRequest(WebView view, HttpAuthHandler handler, String host, String realm) {
+        handler.proceed(handlerUsername, handlerPassword);
     }
 }
